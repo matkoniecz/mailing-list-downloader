b87d27f3024fad23bdb19549f64eb391228d6d3e
Mateusz Konieczny <matkoniecz@gmail.com>
2020-08-26 08:59:10 +0200

    skip future queries

--- fetch-files.rb
+++ fetch-files.rb
@@ -6,7 +6,15 @@ end
 ["tagging", "talk"].each do |mailing_list|
   [2018, 2019].each do |year|
     ["January", "February", "March", "April", "May", "June", "July",
-     "September", "October", "November", "December"].each do |month|
+     "September", "October", "November", "December"].each_with_index do |month, month_index|
+       if year == Time.now.year && (month_index + 1) > Time.now.month
+        puts "skipping #{year} #{month} as it is in future"
+        next
+       end
+       if year > Time.now.year
+        puts "skipping #{year} #{month} as it is in future"
+        next
+       end
        base_url = "https://lists.openstreetmap.org/pipermail"
        filename = "#{mailing_list}-#{year}-#{month}.txt.gz"
        command = "curl #{base_url}/#{mailing_list}/#{year}-#{month}.txt.gz > #{filename}"
